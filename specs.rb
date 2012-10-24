272928346e0344eeca62fcb4b30c180068e5b94d
Justin Blake <justin@hentzia.com>
2012-10-23 21:48:46 -0400

    Refactor specs.

--- specs.rb
+++ specs.rb
@@ -1,48 +1,38 @@
 require './life'
 
-describe 'The next generation of a live cell' do
+describe 'The next generation' do
   subject { cell.next_generation }
-  let(:cell) { LiveCell.new }
 
-  context 'with no living neighbors' do
-    before { 8.times { cell.neighbors << DeadCell.new } }
+  context 'of a live cell' do
+    let(:cell) { LiveCell.new }
+    
+    it 'dies if it had no living neighbors' do
+      8.times { cell.neighbors << DeadCell.new }
+      subject.should be_a(DeadCell)
+    end
 
-    it { should be_a(DeadCell) }
-  end
-
-  context 'with one living neighbor' do
-    before do
+    it 'dies if it had one living neighbor' do
       7.times { cell.neighbors << DeadCell.new }
       cell.neighbors << LiveCell.new
+      subject.should be_a(DeadCell)
     end
 
-    it { should be_a(DeadCell) }
-  end
-
-  context 'with two living neighbors' do
-    before do
+    it 'lives if it had two living neighbors' do
       6.times { cell.neighbors << DeadCell.new }
       2.times { cell.neighbors << LiveCell.new }
+      subject.should be_a(LiveCell)
     end
 
-    it { should be_a(LiveCell) }
-  end
-
-  context 'with three living neighbors' do
-    before do
+    it 'lives if it had three living neighbors' do
       5.times { cell.neighbors << DeadCell.new }
       3.times { cell.neighbors << LiveCell.new }
+      subject.should be_a(LiveCell)
     end
 
-    it { should be_a(LiveCell) }
-  end
-
-  context 'with four living neighbors' do
-    before do
+    it 'dies if it had more than three living neighbors' do
       4.times { cell.neighbors << DeadCell.new }
       4.times { cell.neighbors << LiveCell.new }
+      subject.should be_a(DeadCell)
     end
-
-    it { should be_a(DeadCell) }
   end
 end

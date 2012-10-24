30cd4215ca83590f60675f876057df0d26f70b09
Justin Blake <justin@hentzia.com>
2012-10-23 21:08:58 -0400

    Break "fewer than two live..." spec up into two.

--- specs.rb
+++ specs.rb
@@ -4,8 +4,18 @@ describe 'The next generation of live cell' do
   subject { cell.next_generation }
   let(:cell) { LiveCell.new }
 
-  context 'with fewer than two live neighbors' do
+  context 'with no living neighbors' do
     before { 8.times { cell.neighbors << DeadCell.new } }
+
+    it { should be_a(DeadCell) }
+  end
+
+  context 'with one living neighbor' do
+    before do
+      7.times { cell.neighbors << DeadCell.new }
+      cell.neighbors << LiveCell.new
+    end
+
     it { should be_a(DeadCell) }
   end
 end
